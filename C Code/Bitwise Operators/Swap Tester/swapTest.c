ba0807900eaedef86a0f645b5ab2ad069e1eacca
Tommy <tcm257@yahoo.com>
2021-05-12 09:25:53 -0500

    add CL arguments

--- C Code/Bitwise Operators/Swap Tester/swapTest.c
+++ C Code/Bitwise Operators/Swap Tester/swapTest.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "swap.h"
 
-int main(void)
+int main(int argc, char* argv[])
 {
     printf("\n");
 
     int num1, num2;
 
-    printf("Enter two numbers, seperated by a space: ");
-        scanf("%i %i", &num1, &num2);
-        printf("\n");
+    if(argc > 1) // numbers inputted
+    {
+        num1 = strtol(argv[1], NULL, 10);
+        num2 = strtol(argv[2], NULL, 10);
+    }
+    else // numbers from stdin
+    {
+        printf("Enter two numbers, seperated by a space: ");
+            scanf("%i %i", &num1, &num2);
+            printf("\n");
+    }
 
     printf("Before: \n");
         printf("\tnum1: %i\n", num1);

5ef00f8fa8df167312dada933692814e603ae2da
Tommy <tcm257@yahoo.com>
2021-05-13 03:30:28 -0500

    rename "C Code" to "src"

--- src/Number to Word/numToWord.c
+++ src/Number to Word/numToWord.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 
 #define NUMBER_LENGTH (10 + 1) // length + 1 for null terminator ('\0')
-#define println() printf("\n")
 #define charisNumber(c) (c >= '0' && c <= '9')
 
 const char* wordMap[10] = {
@@ -29,16 +28,16 @@ int main(void)
 {
 	char number[NUMBER_LENGTH];
 
-	printf("\nEnter a number (max %i digits): ", NUMBER_LENGTH-1);
+	printf("\nEnter a number (max %i digits): ", NUMBER_LENGTH - 1);
 		scanf("%10s", number);
 
-	println();
+	printf("\n");
 
 	for(int index = 0; number[index]/*index<NUMBER_LENGTH*/; index++)
 		if(charisNumber(number[index]))
 			printf("%s ", wordMap[charToInt(number[index])]);
 
-	println();
+	printf("\n");
 
 	return 0;
 }
